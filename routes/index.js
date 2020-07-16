249b771678d02e088ba081a80f88dd1c8af0e348
860 <yin@860s-MacBook-Pro.local>
2020-07-15 22:47:30 -0400

    create to database functionality added

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require("../middleware/Auth");
 
+const Story = require("../models/Story");
 //@desc Login/Landing
 //@route GET /
-router.get("/", (req, res) => {
+router.get("/", ensureGuest, (req, res) => {
   res.render("login", {
     layout: "login",
   });
 });
 
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard");
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    });
+    console.log(stories);
+  } catch (err) {
+    console.log(err);
+    res.render("error/500");
+  }
 });
 
 module.exports = router;
