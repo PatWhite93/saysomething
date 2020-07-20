7b3ccdbb04db48ec88b45241d1d74f31b530b2f9
860 <yin@860s-MacBook-Pro.local>
2020-07-20 15:22:27 -0400

    CRUD functionality implemented

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
 const connectDB = require("./config/db");
 const passport = require("passport");
 const session = require("express-session");
@@ -22,13 +23,41 @@ const app = express();
 // Body Parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Method Override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
 // Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+// Handlebars Helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs");
 
 // Handlebars
-app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.engine(
+  ".hbs",
+  exphbs({
+    helpers: { formatDate, stripTags, truncate, editIcon, select },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
 app.set("view engine", ".hbs");
 
 //Sessions
@@ -44,6 +73,12 @@ app.use(
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// set global variable
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
 
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.get("/dashboard", ensureAuth, async (req, res) => {
       name: req.user.firstName,
       stories,
     });
-    console.log(stories);
+    // console.log(stories);
   } catch (err) {
     console.log(err);
     res.render("error/500");
